0c2dbe77808ddf4f1941f4981d2f70ee4828bf9a
Roshan <roshansingh80067@gmail.com>
2025-07-12 15:23:03 +0530

    first commit

---
+++ src/assets/data/carData.js
@@ -0,0 +1,141 @@
+// import all images from assets/images directory
+import img01 from "../all-images/cars-img/ScorpioN.png";
+import img02 from "../all-images/cars-img/tribe.png";
+import img03 from "../all-images/cars-img/bmw-offer.png";
+import img04 from "../all-images/cars-img/nissan-offer.png";
+import img05 from "../all-images/cars-img/offer-toyota.png";
+import img06 from "../all-images/cars-img/Maruti.png";
+import img07 from "../all-images/cars-img/amaze.png";
+import img08 from "../all-images/cars-img/classic.png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "Mahindra",
+    rating: 112,
+    carName: "Mahindra ScorpioN",
+    imgUrl: img01,
+    model: "Model 3",
+    price: 3500,
+    speed: "80kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 2,
+    brand: "Hyundai",
+    rating: 102,
+    carName: "Hyundai amaze",
+    imgUrl: img02,
+    model: "Model-2019",
+    price: 2500,
+    speed: "80kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 3,
+    brand: "Maruti Suzuki",
+    rating: 132,
+    carName: "Maruti Suzuki Dzire",
+    imgUrl: img03,
+    model: "Model-2022",
+    price: 3500,
+    speed: "80kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 4,
+    brand: "Hyundai",
+    rating: 102,
+    carName: "Hyundai aura",
+    imgUrl: img04,
+    model: "Model-2022",
+    price: 3500,
+    speed: "80kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 5,
+    brand: "Maruti Suzuki",
+    rating: 94,
+    carName: "Maruti Suzuki wagno r",
+    imgUrl: img05,
+    model: "Model-2022",
+    price: 4000,
+    speed: "70kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 6,
+    brand: "Maruti Suzuki",
+    rating: 119,
+    carName: "Maruti Suzuki Swifte",
+    imgUrl: img06,
+    model: "Model-2022",
+    price: 3000,
+    speed: "80kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 7,
+    brand: "Hyundai",
+    rating: 82,
+    carName: "Hyundai amaze",
+    imgUrl: img07,
+    model: "Model 3",
+    price: 3000,
+    speed: "80kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 8,
+    brand: "Mahindra",
+    rating: 52,
+    carName: "Mahindra classic",
+    imgUrl: img08,
+    model: "Model 3",
+    price: 3000,
+    speed: "80kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+];
+
+export default carData;
